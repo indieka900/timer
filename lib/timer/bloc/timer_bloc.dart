05b087ec99f29cb54ee8309f28ff7ab4c9cb7119
indieka900 <indiekaj@gmail.com>
2022-10-27 13:46:27 +0300

    Timer

---
+++ lib/timer/bloc/timer_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter_timer/ticker.dart';
+
+part 'timer_event.dart';
+part 'timer_state.dart';
+
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  static int _duration = 60;
+  final Ticker _ticker;
+
+  StreamSubscription<int>? _tickerSubscription;
+
+  TimerBloc({required Ticker ticker})
+      : _ticker = ticker,
+        super(TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
+    emit(TimerRunInProgress(event.duration));
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: event.duration)
+        .listen((duration) => add(TimerTicked(duration: duration)));
+  }
+}
