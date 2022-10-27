e3463cc6ecc3209abe785bbdd3528895dab3d5ee
indieka900 <indiekaj@gmail.com>
2022-10-27 14:38:44 +0300

    Timer

--- lib/timer/view/timer_page.dart
+++ lib/timer/view/timer_page.dart
@@ -22,6 +22,43 @@ class TimerView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Flutter Timer'),
+      ),
+      body: Stack(
+        children: [
+          //const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100),
+                child: Center(
+                  child: TimerText(),
+                ),
+              ),
+              //Actions(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minuteStr = ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondStr = (duration % 60).floor().toString().padLeft(2, '0');
+    return Text(
+      '$minuteStr : $secondStr',
+      style: Theme.of(context).textTheme.headline1,
+    );
   }
 }
